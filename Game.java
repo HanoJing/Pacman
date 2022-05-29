d5a07e2f88445a7529a00dacb29afd0baef6c2b6
EdenBernhard <105359952+EdenBernhard@users.noreply.github.com>
2022-05-29 17:24:00 +0200

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,12 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Game {
+
+	private String player;
+	private int score;
+	private int highscore;
 	
 	
+		
 	public static void main(String[] args) {
 		
 		new GUI();
 		
 	}
+	
+	public void showStartscreen(Graphics g) {
+		
+		player = "Eden";	
+		
+		g.setColor(Color.WHITE);
+		
+		g.drawString("Spieler: "+player, 750, 50);
+		
+	}
+
 	
 	
 
