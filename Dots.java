fa8a78a7d585c960c29211a41e3136cb51db8ce1
EdenBernhard <105359952+EdenBernhard@users.noreply.github.com>
2022-06-12 22:04:58 +0200

    Add files via upload

--- Dots.java
+++ Dots.java
@@ -5,20 +5,39 @@ public class Dots {
 	public int size = 5;
 	public int x;
 	public int y;
-
-	public Dots() {
-
+	public int[] xW = new int[27];
+	public int[] yW = new int[15];
+	public Gamescreen gs;
+	
+	public Dots(Gamescreen gs) {
+		
+		this.gs = gs;
+		
+		int i = 0;
+		int j = 0;
+				
+		for(x = -25; x < 1325; x+=50) {
+			xW[i] = x;
+			i++;
+		}
+		
+		for(y = -25; y < 725; y+=50) {
+			yW[j] = y;
+			j++;
+		}
 	}
 	
 	public void drawDots(Graphics g) {
-		
-		g.setColor(Color.WHITE);
-		
-		for(y = 25; y < 730-80; y+=50) {
-			for(x = 25; x < 1260-20; x+=50) {
+				
+		for(y = 25; y < 650; y+=50) {
+			for(x = 25; x < 1240; x+=50) {
+				g.setColor(Color.WHITE);
 				g.fillOval(x,y,size,size);
-
 			}
 		}
 	}
+		
+		
+				
+
 }
--- Gamescreen.java
+++ Gamescreen.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class Gamescreen extends JPanel{
 
 	private KeyboardFocusManager manager;
-		
+	
 	private Spielfigur pacMan;
 	
 	public boolean kollision = false;
@@ -38,24 +38,31 @@ public class Gamescreen extends JPanel{
 	private Rectangle h27;
 	private Rectangle h28;
 	private Rectangle h29;
+	private Rectangle h30;
 
-	
 	private Dots d;
-
 	private Icons i;
 	private Icons j;
+
+	public boolean initGame = false;
+	
+	public int score = 0;
+	
+	public boolean hit = false;
+
 	
 	public Gamescreen(Frame f) {
 		
   		this.manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-   		this.manager.addKeyEventDispatcher(new KeyDispatcher(f,this));
+   		this.manager.addKeyEventDispatcher(new KeyDispatcher(f, this));
+   	
 
    		h1 = new Rectangle(50,50,155,105);
     	h2 = new Rectangle(250,50,305,105);
     	h3 = new Rectangle(600,8,55,145);
     	h4 = new Rectangle(700,50,305,105);
     	h5 = new Rectangle(1050,50,155,105);
-		h6 = new Rectangle(50,500,155,105);
+    	h6 = new Rectangle(50,500,155,105);
 		h7 = new Rectangle(250,500,305,105);
 		h8 = new Rectangle(600,500,55,150);
 		h9 = new Rectangle(700,500,305,105);
@@ -79,24 +86,32 @@ public class Gamescreen extends JPanel{
 		h27 = new Rectangle(900,250,55,55);
 		h28 = new Rectangle(1050, 200, 55,255); //N
 		h29 = new Rectangle(1150,200,55,255);
+		h30 = new Rectangle(1100,300,55,105);
 
-
-    	d = new Dots();
-		i = new Icons();
+    	
+    	d = new Dots(this);
+    	i = new Icons();
 		j = new Icons();
-
-		pacMan = new Spielfigur(615,315,30,30,380,360,Color.YELLOW);
-
+    	
+    	pacMan = new Spielfigur(615,315,30,30,380,360,Color.YELLOW);
 	}
     	
     public void paintComponent(Graphics g) {
     	super.paintComponent(g);
     	
     	setBackground(Color.BLACK);
-
+    	
+    	if(initGame == false) {
+    		
+    		g.setColor(Color.WHITE);
+			g.drawRoundRect(480,325,230,40,10,10);
+    		g.drawString("Druecke Space/Enter um zu Starten!",500,350);
+     
+    	}else if(initGame == true) {
+    		
     		d.drawDots(g);
-    	    	
-    		h1.drawRectangle(g);
+     	      		    		    			
+    	    h1.drawRectangle(g);
     		h2.drawRectangle(g);
     		h3.drawRectangle(g);
     		h4.drawRectangle(g);
@@ -125,6 +140,10 @@ public class Gamescreen extends JPanel{
 			h27.drawRectangle(g);
 			h28.drawRectangle(g);
 			h29.drawRectangle(g);
+			h30.drawRectangle(g);
+			
+			g.setColor(Color.WHITE);
+			g.drawString("Score: "+score,600,680);
 
 			pacMan.drawSpielfigur(g);
 
@@ -132,9 +151,11 @@ public class Gamescreen extends JPanel{
 
 			i.drawCherry(g);
 			j.drawStrawberry(g);
-    		
+			
+	
     	}
-
+    
+    }
    
     public Spielfigur getPacMan() {
 		return pacMan;
@@ -143,72 +164,87 @@ public class Gamescreen extends JPanel{
 	public void checkKollision() {
 		
 		if(h1.x<=pacMan.startX && (h1.x+h1.width)>pacMan.startX && h1.y<=pacMan.startY && (h1.y+h1.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h2.x<=pacMan.startX && (h2.x+h2.width)>pacMan.startX && h2.y<=pacMan.startY && (h2.y+h2.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h3.x<=pacMan.startX && (h3.x+h3.width)>pacMan.startX && h3.y<=pacMan.startY && (h3.y+h3.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h4.x<=pacMan.startX && (h4.x+h4.width)>pacMan.startX && h4.y<=pacMan.startY && (h4.y+h4.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h5.x<=pacMan.startX && (h5.x+h5.width)>pacMan.startX && h5.y<=pacMan.startY && (h5.y+h5.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h6.x<=pacMan.startX && (h6.x+h6.width)>pacMan.startX && h6.y<=pacMan.startY && (h6.y+h6.height)>=pacMan.startY+pacMan.heigth) {
-		kollision = true;
+				kollision = true;
 		}else if(h7.x<=pacMan.startX && (h7.x+h7.width)>pacMan.startX && h7.y<=pacMan.startY && (h7.y+h7.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h8.x<=pacMan.startX && (h8.x+h8.width)>pacMan.startX && h8.y<=pacMan.startY && (h8.y+h8.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h9.x<=pacMan.startX && (h9.x+h9.width)>pacMan.startX && h9.y<=pacMan.startY && (h9.y+h9.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
-		}else if(h10.x<=pacMan.startX && (h10.x+h10.width)>pacMan.startX && h10.y<=pacMan.startY && (h10.y+h10.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
+     	}else if(h10.x<=pacMan.startX && (h10.x+h10.width)>pacMan.startX && h10.y<=pacMan.startY && (h10.y+h10.height)>=pacMan.startY+pacMan.heigth) {
+     			kollision = true;
 		}else if(h11.x<=pacMan.startX && (h11.x+h11.width)>pacMan.startX && h11.y<=pacMan.startY && (h11.y+h11.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h12.x<=pacMan.startX && (h12.x+h12.width)>pacMan.startX && h12.y<=pacMan.startY && (h12.y+h12.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h13.x<=pacMan.startX && (h13.x+h13.width)>pacMan.startX && h13.y<=pacMan.startY && (h13.y+h13.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h14.x<=pacMan.startX && (h14.x+h14.width)>pacMan.startX && h14.y<=pacMan.startY && (h14.y+h14.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h15.x<=pacMan.startX && (h15.x+h15.width)>pacMan.startX && h15.y<=pacMan.startY && (h15.y+h15.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h16.x<=pacMan.startX && (h16.x+h16.width)>pacMan.startX && h16.y<=pacMan.startY && (h16.y+h16.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h17.x<=pacMan.startX && (h17.x+h17.width)>pacMan.startX && h17.y<=pacMan.startY && (h17.y+h17.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h18.x<=pacMan.startX && (h18.x+h18.width)>pacMan.startX && h18.y<=pacMan.startY && (h18.y+h18.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h19.x<=pacMan.startX && (h19.x+h19.width)>pacMan.startX && h19.y<=pacMan.startY && (h19.y+h19.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h20.x<=pacMan.startX && (h20.x+h20.width)>pacMan.startX && h20.y<=pacMan.startY && (h20.y+h20.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h21.x<=pacMan.startX && (h21.x+h21.width)>pacMan.startX && h21.y<=pacMan.startY && (h21.y+h21.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h22.x<=pacMan.startX && (h22.x+h22.width)>pacMan.startX && h22.y<=pacMan.startY && (h22.y+h22.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h23.x<=pacMan.startX && (h23.x+h23.width)>pacMan.startX && h23.y<=pacMan.startY && (h23.y+h23.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h24.x<=pacMan.startX && (h24.x+h24.width)>pacMan.startX && h24.y<=pacMan.startY && (h24.y+h24.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h25.x<=pacMan.startX && (h25.x+h25.width)>pacMan.startX && h25.y<=pacMan.startY && (h25.y+h25.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h26.x<=pacMan.startX && (h26.x+h26.width)>pacMan.startX && h26.y<=pacMan.startY && (h26.y+h26.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h27.x<=pacMan.startX && (h27.x+h27.width)>pacMan.startX && h27.y<=pacMan.startY && (h27.y+h27.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h28.x<=pacMan.startX && (h28.x+h28.width)>pacMan.startX && h28.y<=pacMan.startY && (h28.y+h28.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
 		}else if(h29.x<=pacMan.startX && (h29.x+h29.width)>pacMan.startX && h29.y<=pacMan.startY && (h29.y+h29.height)>=pacMan.startY+pacMan.heigth) {
-			kollision = true;
+				kollision = true;
+		}else if(h30.x<=pacMan.startX && (h30.x+h30.width)>pacMan.startX && h30.y<=pacMan.startY && (h30.y+h30.height)>=pacMan.startY+pacMan.heigth) {
+				kollision = true;
 		}else {
-			kollision = false;
+				kollision = false;
+			}
 		}
-	}
-
-    /*public void initGame() {
+	
+    public void initGame() {
     	
     	if(KeyDispatcher.spacePressed == true || KeyDispatcher.enterPressed == true) {
-    		initGame = true;
+    		this.initGame = true;
     	}
-    }*/
+    }
+    
+    public void score() {
+      	    
+    	for(int j = 1; j < d.yW.length-1; j++) {
+    		for(int i = 1; i < d.xW.length-1; i++) {    	
+    			if(pacMan.startY < d.yW[j] && pacMan.startY < d.yW[j+1] && pacMan.startY > d.yW[j-1] && pacMan.startX < d.xW[i] && pacMan.startX < d.xW[i+1] && pacMan.startX > d.xW[i-1]) {
+    				score++;
+      				hit = true;
+    			}
+    		}
+    	}
+    }
+	    
 }
