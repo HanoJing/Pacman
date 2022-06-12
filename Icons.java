1dc8bd231f2b2f9f1e5c5e387b83b2f7241e1468
Melisschen <melissa.freund2005@gmail.com>
2022-06-12 18:47:21 +0200

    Icons <3

---
+++ Icons.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+public class Icons {
+
+    public void drawCherry(Graphics g) {
+
+        g.setColor(Color.red);
+        g.fillRect(1180, 680, 8, 2);
+        g.fillRect(1178, 682, 12, 2);
+        g.fillRect(1178, 684, 10, 2);
+        g.fillRect(1178, 686, 2, 2);
+        g.fillRect(1182, 686, 4, 2);
+        g.fillRect(1178, 688, 4, 2);
+        g.fillRect(1184, 688, 2, 2);
+        g.fillRect(1180, 690, 6, 2);
+        g.fillRect(1190, 684, 8, 2);
+        g.fillRect(1188, 686, 12, 2);
+        g.fillRect(1188, 688, 12, 2);
+        g.fillRect(1188, 690, 2, 2);
+        g.fillRect(1192, 690, 8, 2);
+        g.fillRect(1188, 692, 4, 2);
+        g.fillRect(1194, 692, 6, 2);
+        g.fillRect(1190, 694, 8, 2);
+
+        g.setColor(Color.orange);
+        g.fillRect(1188, 678, 2, 2);
+        g.fillRect(1190, 676, 4, 2);
+        g.fillRect(1194, 674, 8, 2);
+        g.fillRect(1198, 672, 4, 2);
+        g.fillRect(1196, 676, 2, 4);
+        g.fillRect(1194, 680, 2, 2);
+        g.fillRect(1192, 682, 2, 2);
+
+        g.setColor(Color.white);
+        g.fillRect(1180, 686, 2, 2);
+        g.fillRect(1182, 688, 2, 2);
+        g.fillRect(1190, 690, 2, 2);
+        g.fillRect(1192, 692, 2, 2);
+
+
+    }
+
+    public void drawStrawberry(Graphics g){
+
+        g.setColor(Color.red);
+
+        g.setColor(Color.green);
+        g.fillRect(1235,672,2,2);
+
+        g.setColor(Color.white);
+    }
+}
