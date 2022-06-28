96a7b5ea85a9895fd169327a504eca561b30c9db
Melisschen <melissa.freund2005@gmail.com>
2022-06-28 10:39:18 +0200

    Code Optimierung, Schleifchen Anpassung, Frame Versuche

--- src/Rectangle_Girly.java
+++ src/Rectangle_Girly.java
@@ -16,28 +16,25 @@ public class Rectangle_Girly {
     public void drawRectangle_Girly(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(410,250,30,110);
+        g.fillRoundRect(this.x,this.y,this.width,this.height,10,10);
         g.setColor(Color.MAGENTA);
         g.drawRoundRect(this.x,this.y,this.width,this.height,10,10);
-        g.drawLine(405,250,427,275);
-        g.drawLine(427,275,450,250);
-        g.drawLine(405,320,427,345);
-        g.drawLine(427,345,450,320);
-        g.drawLine(639,354,705,418);
-        g.drawLine(605,390,667,455);
+        g.drawLine(589,354,655,418);
+        g.drawLine(555,390,617,455);
         g.setColor(Color.BLACK);
-        g.fillRoundRect(this.x,this.y,this.width,this.height,10,10);
-        g.fillRoundRect(560,200,100,49,10,10);
-        g.fillRoundRect(560,306,100,49,10,10);
-        g.fillRoundRect(750,200,55,155,10,10);
-        g.fillRoundRect(850,300,55,155,10,10);
+        //g.fillRoundRect(this.x,this.y,this.width,this.height,10,10);
+        //g.fillRoundRect(560,200,100,49,10,10);
+        //g.fillRoundRect(560,306,100,49,10,10);
+        //g.fillRoundRect(750,200,55,155,10,10);
+        //g.fillRoundRect(850,300,55,155,10,10);
         g.fillRoundRect(50,150,105,55,10,10);
         g.fillRoundRect(50,450,105,55,10,10);
         g.fillRoundRect(450,500,105,55,10,10);
         g.fillRoundRect(500,500,55,153,10,10);
         g.fillRoundRect(500,4,55,149,10,10);
-        g.fillRoundRect(664,414,40,40,10,10);
-        g.fillRoundRect(600,350,40,40,10,10);
-        g.fillRoundRect(700,4,55,149,10,10);
+        //g.fillRoundRect(664,414,40,40,10,10);
+        //g.fillRoundRect(600,350,40,40,10,10);
+        g.fillRoundRect(700,4,49,149,10,10);
         g.fillRoundRect(700,500,55,152,10,10);
         g.fillRoundRect(1100,150,105,55,10,10);
         g.fillRoundRect(1100,450,105,55,10,10);
@@ -49,7 +46,5 @@ public class Rectangle_Girly {
         g.fillRect(1205,250,48,55);
         g.fillRect(1205,350,48,55);
         g.fillRect(1205,550,48,55);
-        g.drawLine(405,251,405,320);
-        g.drawLine(450,251,450,320);
     }
 }
